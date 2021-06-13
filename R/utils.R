20a3bc0d2ab2a452c445567fb3bf9caf2e86e6b0
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-06-14 01:34:58 +0300

    added utility function to plot adj matrix

---
+++ R/utils.R
@@ -0,0 +1,29 @@
+#' Plot Adjacency Matrix
+#'
+#' Produces a simple plot of the supplied adjacency matrix.
+#'
+#' @param mat matrix to be plotted.
+#'
+#' @return None.
+#'
+#' @examples
+#' data(mid_mat)
+#' checkmat(mid_mat)
+#'
+#' @export
+#'
+# Function that produces a simple plot of the supplied adjacency matrix.
+checkmat <- function(mat){
+  adj.plot<-apply(mat,1,rev); n.nodes<-nrow(adj.plot)
+  par(mar=c(0,0,0,0)+0.2); plot.new(); plot.window(xlim=c(0.5,n.nodes+0.5),ylim=c(0.5,n.nodes+0.5),xaxs="i",yaxs="i")
+
+  for(i in 1:n.nodes){
+    for(j in 1:n.nodes){
+      if(adj.plot[i,j]==1){polygon(y=c(i-0.5,i+0.5,i+0.5,i-0.5),x=c(j-0.5,j-0.5,j+0.5,j+0.5),
+                                   col="black",
+                                   border=NA)}
+    }
+  }
+  abline(v = c(0.5, seq(1, n.nodes) + 0.5), col = gray(0.9), lwd = 0.5)
+  abline(h = c(0.5, seq(1, n.nodes) + 0.5), col = gray(0.9), lwd = 0.5)
+}
