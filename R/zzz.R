50e8c73e9928dbee1bbb29012f0c5e4a583fd18f
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2022-06-15 13:48:05 +0300

    updated dates for 2022 workshop

--- R/zzz.R
+++ R/zzz.R
@@ -7,5 +7,5 @@
     'Date:     ', desc$Date, '\n',
     'Author:   Ted Hsuan Yun Chen (Aalto University and University of Helsinki)\n'
   )
-  packageStartupMessage("This package contains data and functions needed for the 2021 PolNet ERGM workshop.")
+  packageStartupMessage("This package contains data and functions needed for the PolNet ERGM workshop.")
 }
