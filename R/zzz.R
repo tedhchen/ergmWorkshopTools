6bb0f634c6b84bf48d0e67188033bda1377a048c
tedhchen <ted.hsuanyun.chen@gmail.com>
2023-06-19 17:11:15 +0300

    updated affiliation to gmu

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
     'Package:  ergmWorkshopTools\n',
     'Version:  ', desc$Version, '\n',
     'Date:     ', desc$Date, '\n',
-    'Author:   Ted Hsuan Yun Chen (Aalto University and University of Helsinki)\n'
+    'Author:   Ted Hsuan Yun Chen (George Mason University))\n'
   )
   packageStartupMessage("This package contains data and functions needed for the PolNet ERGM workshop.")
 }
